8d73dc0666b2ac367c53cd70cb8e41480a2576a0
Victor Marcolino <marcolino.victor@gmail.com>
2021-08-23 17:08:49 -0300

    fix: allow cors

--- src/golang/pkg/handlers/setupRouter.go
+++ src/golang/pkg/handlers/setupRouter.go
@@ -3,7 +3,9 @@ package handlers
 import "github.com/gin-gonic/gin"
 
 func SetupRouter() *gin.Engine {
+
 	route := gin.Default()
+	route.Use(CORSMiddleware)
 	api := route.Group("/api")
 	{
 		api.POST("/create", CreateVoting)
@@ -14,4 +16,11 @@ func SetupRouter() *gin.Engine {
 	}
 
 	return route
+}
+func CORSMiddleware(c *gin.Context) {
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+	c.Next()
 }
