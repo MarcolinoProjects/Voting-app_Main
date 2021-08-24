425e596f4fc9d75ecfcf9a36eb326f0defbe4583
Victor Marcolino <marcolino.victor@gmail.com>
2021-08-23 22:46:15 -0300

    feat: simple nodejs API with socket.io listening to events queue

---
+++ src/nodejs/src/app/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const Server = require('socket.io');
+const app = express();
+const expressServer = app.listen(9000);
+const io = Server(expressServer, {path: '/mysubdir/socket.io', cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }});
+io.on('connection', (socket)=>{
+    socket.on('messageToServer', (dataFromClient)=>{
+        console.log(dataFromClient)
+    })
+});
+const rabbit = require("./rabbitmq");
+rabbit.bindSocketIO(io)
---
+++ src/nodejs/src/app/rabbitmq.js
@@ -0,0 +1,37 @@
+var amqp = require('amqplib/callback_api');
+
+const host = 'amqp://rabbitmq:rabbitmq@localhost'
+class Rabbitmq {
+    socketIO=undefined
+    constructor() {
+        const reference = this;
+        amqp.connect(host, function(error0, connection) {
+            if (error0) {
+                throw error0;
+            }
+            connection.createChannel(function(error1, channel) {
+                if (error1) {
+                    throw error1;
+                }
+                const queue = 'events';
+                channel.assertQueue(queue, {
+                    durable: false
+                });
+                channel.consume(queue, function(msg) {
+                    if (reference.socketIO){
+                        const votingSession = JSON.parse(msg.content.toString())
+                        reference.socketIO.emit(votingSession.uuid, msg.content.toString())
+                    }
+                }, {
+                    noAck: true
+                });
+            });
+        });
+    }
+    bindSocketIO(socketIO){
+        this.socketIO=socketIO
+    }
+}
+
+const rabbit = new Rabbitmq()
+module.exports = rabbit;
