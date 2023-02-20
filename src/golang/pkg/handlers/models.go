b18110bc8526b32e8585842de5af65775d2d8fb0
Victor Marcolino <Victor.Marcolino@swisscom.com>
2023-02-20 15:19:21 +0100

    Move Go files to new repository

--- src/golang/pkg/handlers/models.go
+++
@@ -1,9 +0,0 @@
-package handlers
-
-type CandidateInfo struct {
-	SessionUuid   string `uri:"sessionUuid" binding:"required,uuid"`
-	CandidateUuid string `uri:"candidateUuid" binding:"required,uuid"`
-}
-type SessionInfo struct {
-	SessionUuid string `uri:"sessionUuid" binding:"required,uuid"`
-}
